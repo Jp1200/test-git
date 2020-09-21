da6b8941901c889a62d0466df7ddfd158c0f1dc7
Jp1200 <jprkr96@gmail.com>
2020-09-20 18:06:08 -0700

    Updates

---
+++ precache-manifest.4c1836eb27ce7b741bc0a9bf8312388d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10c627788cc0eb7c9ee8633491f552ea",
+    "url": "/test-git/index.html"
+  },
+  {
+    "revision": "f2cb0c9ec952a2560aef",
+    "url": "/test-git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c769ac400ea0b168ac83",
+    "url": "/test-git/static/js/2.ffdf3435.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-git/static/js/2.ffdf3435.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2cb0c9ec952a2560aef",
+    "url": "/test-git/static/js/main.3626400e.chunk.js"
+  },
+  {
+    "revision": "e0d17ff758131904bcae",
+    "url": "/test-git/static/js/runtime-main.bed85489.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-git/static/media/logo.5d5d9eef.svg"
+  }
+]);
